d15492ac7db3e5815ca5c83826609853d5eca51e
phuohq2009 <phuochq2009@gmail.com>
2024-11-12 20:24:18 +0700

    cuoikhoa

--- js/login.js
+++ js/login.js
@@ -1,32 +1,31 @@
 function myFunction() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function login() {
+  event.preventDefault();
+  let emailData = document.getElementById("email").value;
+  let passwordData = document.getElementById("password").value;
+
+  if (emailData === "" || passwordData === "") {
+    alert("Vui lòng nhập đầy đủ thông tin");
+    return;
+  }
+
+  console.log(emailData, passwordData);
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === emailData && users[i].password === passwordData) {
+      const isLogin = true;
+      localStorage.setItem("isLogin", isLogin);
+
+      alert("Đăng nhập thành công");
+      return;
     }
   }
-function login() {
-    event.preventDefault();
-    let emailData = document.getElementById("email").value;
-    let passwordData = document.getElementById("password").value;
-    
-    if (emailData === '' || passwordData === '') {
-        alert('Vui lòng nhập đầy đủ thông tin');
-        return;
-    }
-
-    console.log(emailData, passwordData)
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].email === emailData && users[i].password === passwordData) {
-            const isLogin = true;
-            localStorage.setItem('isLogin', isLogin);
-
-            alert('Đăng nhập thành công')
-            return;
-        }
-
-    }
-    alert('email hoặc mật khẩu không đúng')
+  alert("email hoặc mật khẩu không đúng");
 }
--- js/p.js
+++ js/p.js
@@ -1,30 +1,28 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-image');
+const slides = document.querySelectorAll(".carousel-image");
 const totalSlides = slides.length;
 
 function showSlide(index) {
-    const carousel = document.querySelector('.carousel');
-    
-    currentSlide = (index + totalSlides) % totalSlides;
-    
-    carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
+  const carousel = document.querySelector(".carousel");
+
+  currentSlide = (index + totalSlides) % totalSlides;
+
+  carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
 function changeSlide(step) {
-    showSlide(currentSlide + step);
+  showSlide(currentSlide + step);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    showSlide(currentSlide);
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentSlide);
 });
 
 function buy() {
-    const isLogin = localStorage.getItem('isLogin');
-    if (isLogin) {
-        location.href = 'location.html';
-        
-    } else {
-        alert('Please login to buy')
-       
-    }
+  const isLogin = localStorage.getItem("isLogin");
+  if (isLogin) {
+    location.href = "location.html";
+  } else {
+    alert("Please login to buy");
+  }
 }
